c049d336ec5018ca2e6c948a0c641bc8bf339d6f
Ihor <kohutiv@gmail.com>
2018-02-06 00:20:28 +0200

    + ::findAll();

---
+++ App/Models/Book.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+use App\Db;
+
+
+class Book
+{
+    public $id;
+    public $author;
+    public $alias;
+    public $url;
+    public $book_type;
+    public $class_id;
+    public $year;
+    public $title;
+
+
+    public static function findAll(){
+        $db = new Db();
+
+        return $db->query(
+            'SELECT id, author, alias, url, book_type, class_id, year, title FROM books',
+            'App\Models\Book'
+        );
+    }
+
+
+
+
+
+}
